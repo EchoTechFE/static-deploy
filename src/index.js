dc89fb24724663483e420f1bb3e6b7eddc4d1608
Liangzhen Zhu <zhuliangzhen@echo.tech>
2022-12-21 22:15:03 +0800

    feat: 添加 cache-control

--- src/index.js
+++ src/index.js
@@ -9,12 +9,38 @@ const deploy = async (src, dist, config) => {
   const files = glob.sync('**/*', {
     cwd: prefix
   })
+
+  console.log(`bucket: ${config.bucket}`)
+  console.log(`dist: ${dist || '/'}`)
+
   for (const file of files) {
     const stream = fs.createReadStream(path.resolve(prefix, file))
     const target = path.join(dist, file)
-    await client.putStream(target, stream, {})
-    console.log(`upload ${file} to ${target}`)
+    const options = {}
+    const MONTH_AGE = 60*60*24*30
+
+    // 上传的文件缓存策略
+    if (file.endsWith('.html')) {
+      options['Cache-Control'] = 'max-age=1'
+    } else if (file.endsWith('service-worker.js')) {
+      // do nothing
+    } else if (file.endsWith('.js')) {
+      options['Cache-Control'] = `max-age=${MONTH_AGE}`
+    } else if (file.endsWith('.css')) {
+      options['Cache-Control'] = `max-age=${MONTH_AGE}`
+    }
+
+    await client.putStream(target, stream, options)
+
+    let msg = `upload ${file}`
+    if (options['Cache-Control']) {
+      msg += `, cache-control: ${options['Cache-Control']}`
+    }
+
+    console.log(msg)
   }
+
+  console.log('✨ deploy done')
 }
 
 module.exports = {
