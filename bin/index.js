3850796c4a9e723d35233efc20bee75836642f50
Liangzhen Zhu <zhuliangzhen@echo.tech>
2022-12-22 13:04:52 +0800

    feat: better args

--- bin/index.js
+++ bin/index.js
@@ -5,13 +5,13 @@ const { deploy } = require('../src')
 
 const cli = cac('static-deploy')
 
-cli.command('deploy').option('--src <source>', 'source directory', {
+cli.command('deploy').option('--src [source]', 'source directory', {
   default: 'dist'
 }).option('--dist [dist]', 'dist directory', {
   default: ''
 }).action((options) => {
-  const src = options['src']
-  const dist = options['dist']
+  const src = typeof options['src'] === 'string' ? options['src'] : ''
+  const dist = typeof options['dist'] === 'string' ? options['dist'] : ''
   const configContent = fs.readFileSync('.aliossrc')
   const config = JSON.parse(configContent)
   deploy(src, dist, config)
