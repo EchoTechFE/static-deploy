3be288b16e00c570e2d73f036ce8c9315782c469
zhuscat <zhudx6512@163.com>
2024-05-07 18:39:22 +0800

    feat: 支持环境变量

--- bin/index.js
+++ bin/index.js
@@ -12,9 +12,39 @@ cli.command('deploy').option('--src [source]', 'source directory', {
 }).action((options) => {
   const src = typeof options['src'] === 'string' ? options['src'] : ''
   const dist = typeof options['dist'] === 'string' ? options['dist'] : ''
-  const configContent = fs.readFileSync('.aliossrc')
-  const config = JSON.parse(configContent)
-  deploy(src, dist, config)
+  if (fs.existsSync('.aliossrc')) {
+    const configContent = fs.readFileSync('.aliossrc')
+    const config = JSON.parse(configContent)
+    deploy(src, dist, config)
+  } else {
+    const accessKeyId = process.env.OSS_ACCESS_KEY_ID
+    const accessKeySecret = process.env.OSS_ACCESS_KEY_SECRET
+    const region = process.env.OSS_REGION
+    const bucket = process.env.OSS_BUCKET
+    if (!accessKeyId) {
+      console.error('OSS_ACCESS_KEY_ID is required')
+      process.exit(1)
+    }
+    if (!accessKeySecret) {
+      console.error('OSS_ACCESS_KEY_SECRET is required')
+      process.exit(1)
+    }
+    if (!region) {
+      console.error('OSS_REGION is required')
+      process.exit(1)
+    }
+    if (!bucket) {
+      console.error('OSS_BUCKET is required')
+      process.exit(1)
+    }
+    const config = {
+      accessKeyId,
+      accessKeySecret,
+      region,
+      bucket
+    }
+    deploy(src, dist, config)
+  }
 })
 
 cli.help()
